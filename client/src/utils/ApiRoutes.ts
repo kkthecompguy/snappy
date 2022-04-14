22707cbaab46a5135ff2150340d4dba356caa184
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-04-14 12:13:49 +0300

    proxy pass frontend

--- client/src/utils/ApiRoutes.ts
+++ client/src/utils/ApiRoutes.ts
@@ -1,5 +1,5 @@
 export const host: string = process.env.REACT_APP_HOST || "http://localhost:3001"
-const baseurl: string = `${host}/api/v1/`;
+const baseurl: string = `/api/v1/`;
 const apiRoutes = {
   registerRoute: `${baseurl}auth/register`,
   loginRoute: `${baseurl}auth/login`,
